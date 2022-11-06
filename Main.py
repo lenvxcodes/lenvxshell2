01aa2fc9eb0ad64ebbd898a39ac15f7d5c2a53f3
Lenvx <111296626+lenvxcodes@users.noreply.github.com>
2022-11-06 18:12:46 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
 """ LenvxShell Remake"""
 
 #Important
-import colorama, ujson, random, platform, os, time, requests, string
+import colorama, ujson, random, platform, os, time, requests, string, readline, psutil
 from colorama import Fore
 from datetime import date
+from os import getpid
+mem_usage = psutil.virtual_memory()
+my_process = psutil.Process(getpid())
 ver = 0.2
 today = date.today()
 d1 = today.strftime("%d/%m/%Y")
@@ -49,7 +52,7 @@ def shell():
     print(f"{d1} -~~~~")
     print("--------------------------------------")
     while True:
-        sytax=input(console["prefix"]).strip()
+        sytax=input(console['prefix']).strip()
         if sytax == "":
             pass
         elif sytax == "hear":
@@ -71,8 +74,11 @@ def shell():
                      {Fore.BLUE} ver {Fore.RESET} >> {Fore.GREEN} version
                      {Fore.BLUE} qu! {Fore.RESET} >> {Fore.GREEN} quits the shell.
                      {Fore.BLUE} kz {Fore.RESET} >> {Fore.GREEN} Change your prefix/PS1
+                     {Fore.BLUE} kx {Fore.RESET} >> {Fore.GREEN} Change your pass
+                     {Fore.BLUE} kc {Fore.RESET} >> {Fore.GREEN} Change your user
                      {Fore.BLUE} feth {Fore.RESET} >> {Fore.GREEN} neofetch but lenvxshell's only
-                     {Fore.BLUE} randomize {Fore.RESET} >> {Fore.GREEN} Hides your text with random letters
+                     {Fore.BLUE} randomize {Fore.RESET} >> {Fore.GREEN} hides text with generated letters
+                     {Fore.BLUE} spamermize {Fore.RESET} >> {Fore.GREEN} spams the the output with selected text
                     {Fore.RESET}
                      """)
         elif sytax == "cl":
@@ -84,14 +90,14 @@ def shell():
              &%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&
            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&
-         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%             {Fore.RED}OS      = {Fore.RESET}{platform.system()}{Fore.GREEN}
-       %%%%%%%%%%%%%%%   %%%%%%.  %%%%%%%%%%%%%%%%            {Fore.RED}Date    = {Fore.RESET}{d1}{Fore.GREEN}
-      %%%%%%%%%%%%%%#  *%%%%%%   %%%%%%%%%%%%%%%%%%&          {Fore.RED}RP      = {Fore.RESET}LenvxREPL{Fore.GREEN}
-     %%%%%%%%%%%%%%#     *%%%,  %%%%%%%%%%%%%%%%%%%%          {Fore.RED}State   = {Fore.RESET}DEV{Fore.GREEN}
-    %%%%%%%%%%%%%%%   %%%#     %%%%%%%%%%%%%%%%%%%%%%         {Fore.RED}Version = {Fore.RESET}0.2{Fore.GREEN}
-    %%%%%%%%%%%%%%%  ,%%%%%*  #.   /%%%%%%%%%%%%%%%%%         {Fore.RED}Update needed: = {Fore.RESET}{updatestate}{Fore.GREEN}
-    %%%%%%%%%%%%%%%  *%%%%/  *    ,#%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%  .%(.    (%%%%%%%%%%%%%%%%%%%%%%%
+         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%             {Fore.RED}OS      = {Fore.BLUE}{platform.system()}{Fore.GREEN}
+       %%%%%%%%%%%%%%%   %%%%%%.  %%%%%%%%%%%%%%%%            {Fore.RED}Date    = {Fore.BLUE}{d1}{Fore.GREEN}
+      %%%%%%%%%%%%%%#  *%%%%%%   %%%%%%%%%%%%%%%%%%&          {Fore.RED}RP      = {Fore.BLUE}LenvxREPL{Fore.GREEN}
+     %%%%%%%%%%%%%%#     *%%%,  %%%%%%%%%%%%%%%%%%%%          {Fore.RED}State   = {Fore.BLUE}DEV{Fore.GREEN}
+    %%%%%%%%%%%%%%%   %%%#     %%%%%%%%%%%%%%%%%%%%%%         {Fore.RED}Version = {Fore.BLUE}0.3{Fore.GREEN}
+    %%%%%%%%%%%%%%%  ,%%%%%*  #.   /%%%%%%%%%%%%%%%%%         {Fore.RED}Update needed: = {Fore.BLUE}{updatestate}{Fore.GREEN}
+    %%%%%%%%%%%%%%%  *%%%%/  *    ,#%%%%%%%%%%%%%%%%%         {Fore.RED}RAM usage = {Fore.BLUE}{mem_usage.used/(1024**3):.2f}/{mem_usage.total/(1024**3):.2f}g{Fore.GREEN}
+    %%%%%%%%%%%%%%%  .%(.    (%%%%%%%%%%%%%%%%%%%%%%%         {Fore.RED}CPU ={Fore.BLUE} {my_process.cpu_percent(interval=0)}%{Fore.GREEN}
      %%%%%%%%%%%%%%,   ,%  /%%%%%%%%%%%%%%%%%%%%%%%%%
      &%%%%%%%%%%%%%%   #  /%%%%%%%%%%%%%%%%%%%%%%%%%
        %%%%%%%%%%%%%%   %%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -112,18 +118,45 @@ def shell():
                     console["prefix"] = newps1
                     with open("data/logindata.json", "w") as cfgf:
                         ujson.dump(console, cfgf, indent=4)
-                        break
+                    break
                 if i + 1 == 3:
                     time.sleep(2)
                     print("Failed all 3 attempts")
                     pass
                 else:
                     time.sleep(2)
-                    print("Try again")           
+                    print("Try again")      
+        elif sytax == "kx":
+            print("KOS lenvxshell edition")
+            for i in range(3):
+                verifypass=input(f"(Password for {console['user']})")
+                if verifypass == console["pass"]:
+                    newps1=input("New password: ")
+                    console["pass"] = newps1
+                    with open("data/logindata.json", "w") as cfgf:
+                        ujson.dump(console, cfgf, indent=4)
+                    break
+        elif sytax == "kc":
+            print("KOS lenvxshell edition")
+            for i in range(3):
+                verifypass=input(f"(Password for {console['user']})")
+                if verifypass == console["pass"]:
+                    newps1=input("New username: ")
+                    console["user"] = newps1
+                    with open("data/logindata.json", "w") as cfgf:
+                        ujson.dump(console, cfgf, indent=4)
+                    break
+                if i + 1 == 3:
+                    time.sleep(2)
+                    print("Failed all 3 attempts")
+                    pass
+                else:
+                    time.sleep(2)
+                    print("Try again")       
         elif sytax == "src":
             print("https://github.com/lenvxcodes/lenvxshell2")
         elif sytax == "ver":
-            print("Version 0.1")
+            print("Version 0.3")
         elif sytax == "clst":
             os.system('clear')
             print(console['welcome'])
@@ -135,9 +168,13 @@ def shell():
         elif sytax == "randomize":
             print("Usage: $~ randomize Im-somewhere-here")
         elif f"{sytax} ".startswith("randomize "):
-            print(f"{randomize + sytax[9:] + randomize}")
+            print(f"{randomize + Fore.YELLOW + sytax[9:] + Fore.RESET + randomize}")
+        elif f"{sytax} ".startswith("spamermize "):
+            for _ in range(1 + input("Times to spam:")):
+                print(sytax[10:])
+        elif sytax == "spamermize":
+            print("Usage: randomize Spammed!")
         else:
             print("Response Invalid.")
-
 randomize =''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.punctuation) for i in range(1000))
 shell()
